8e136b538b88e04bb62007bbaee5ff3180f70685
caprosset <capucinerosset@gmail.com>
2019-11-22 18:45:25 +0100

    Start user routes

--- routes/private/meal-events.js
+++ routes/private/meal-events.js
@@ -8,5 +8,22 @@ router.get('/', (req, res, next) => {
   });
   
 
+//   GET	/meal-events/create	Private route. Renders the meal-views/create form view to add a new event for the current user.
+
+
+// POST	/meal-events/create	Private route. Adds a new event for the current user. Redirects to the meal-views/show-all view (url: /meal-events).
+
+
+// GET	/meal-events/:id	Private route. Renders the meal-views/show view.
+
+
+// GET	/meal-events/:id/edit	Private route. Renders the meal-views/edit form view.
+
+
+// PUT	/meal-events/:id/edit	Private route. Updates the existing event from the current user in the DB. Redirects to the user-views/myevents view.
+
+
+
+// DELETE	/meal-events/:id/delete
 
 module.exports = router;
--- routes/private/users.js
+++ routes/private/users.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// GET	/profile/:id	Private route. Renders user-views/show view.
+router.get('/:id', (req, res, next) => {
+    res.render('user-views/show');
+})
+
+// GET	/profile/:id/edit	Private route. Renders user-views/edit form view.
+
+// PUT	/profile/:id/edit	Private route. Sends edit-profile info to server and updates user in DB. Redirects to the user-views/show view (url: /profile/:id).
+
+// DELETE	/profile/:id/delete
 
 module.exports = router;
