40c1eb666bbab91d1e6960ef8f7c79c6c438b688
caprosset <capucinerosset@gmail.com>
2019-11-22 11:47:11 +0100

    Fix port bug

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const hbs = require('hbs');
 
-var app = express();
+const app = express();
+
+// routers
+const indexRouter = require('./routes/index');
+const siteRouter = require('./routes/site-routes');
+
+// Mongoose configuration
+mongoose.connect('mongodb://localhost/neighbourfood', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Middleware configuration
+app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Authentication
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+  }),
+);
+app.use(cookieParser());
+
+
+
+// ROUTES
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', siteRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
@@ -37,5 +72,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
