9fe8c5be12d9d67d067e639d93b3dc10dfd8a26e
caprosset <capucinerosset@gmail.com>
2019-11-25 19:42:08 +0100

    Display events matching the zipcode entered on the home

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,13 @@ const loginRouter = require('./auth/login');
 const signoutRouter = require('./auth/signout');
 const signupRouter = require('./auth/signup');
 
+// Models
+const MealEvent = require('./../models/MealEvent');
+// const User = require('../../models/User');
+
+// HTML elements
+
+// const result = document.querySelector('#search-results');
 
 
 // * '/auth'
@@ -31,9 +38,27 @@ router.get('/', (req, res, next) => {
   }
 }); 
 
+
 router.get('/search', (req, res, next) => {
-  console.log('req.query ->', req.query);
-  res.send(req.query);
+  // console.log('req.query ->', req.query);
+  const { zipcode: enteredZipcode } = req.query;
+
+  MealEvent.find()
+  .populate('host')
+  .then( (allMealsFromDB) => {
+    // console.log('MEEAAAALSSSS', allMealsFromDB);
+
+    const eventsNearZipcode = allMealsFromDB.filter( oneMeal => {  
+      // console.log(oneMeal.host);
+      if(oneMeal.host !== null) {
+        return oneMeal.host.address.zipcode === enteredZipcode;
+      }
+    }) 
+    // console.log(eventsNearZipcode);
+    res.render('index', { eventsNearZipcode });
+  })
+  .catch( (err) => console.log(err));
 }); 
+
 
 module.exports = router;
