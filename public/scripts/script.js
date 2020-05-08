b767c65001396cf2c03e3aceb372411b0ce51e86
caprosset <capucinerosset@gmail.com>
2020-05-08 16:31:26 +0200

    Add alert message on homepage

---
+++ public/scripts/script.js
@@ -0,0 +1,3 @@
+if(window.location.pathname === '/') {
+  alert("Welcome to my app Neighbourfood! To start playing around, please signup and choose the zipcode '08003' (the current database only content events in that area of Barcelona). Thanks for your understanding!")
+}
